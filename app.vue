81d5a4752191c74a5ccc8d6609683145ebb060bc
Cyril Suzat <cyril.suzat@me.com>
2022-11-25 14:46:53 +0100

    Setting up typography

--- app.vue
+++ app.vue
@@ -5,9 +5,9 @@
 </template>
 
 <style lang="sass">
-  body
-    background-color: $primary
-
-    +desktop
-      background-color: $secondary
+body
+  -moz-osx-font-smoothing: grayscale
+  -webkit-font-smoothing: antialiased
+  font-family: BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif
+  margin: 0
 </style>
