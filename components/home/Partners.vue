99ab3ef9a0289b4b4f23ee88a0f419df048721f8
Cyril Suzat <cyril.suzat@me.com>
2023-02-24 16:14:09 +0100

    WIP: Partners

---
+++ components/home/Partners.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <ul class="partners">
+      <li>
+        <NuxtLink to="https://www.freude-herrscht.ch/fran%C3%A7ais/" target="_blank">
+          <img src="~/assets/images/partners/freude-herrscht.png" alt="Freude herrscht" />
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="https://www.baechli-bergsport.ch/fr" target="_blank">
+          <img src="~/assets/images/partners/baechli.png" alt="Bächli" />
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="https://maiday.ch/" target="_blank">
+          <img src="~/assets/images/partners/maiday.png" alt="Maiday" />
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="https://www.compex.com/ch/fr/" target="_blank">
+          <img src="~/assets/images/partners/compex.png" alt="Compex" />
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="https://bgcm.ch/" target="_blank">
+          <img src="~/assets/images/partners/bgcm.png" alt="BGCM" />
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="https://bonvin1858.ch/" target="_blank">
+          <img src="~/assets/images/partners/bonvin.png" alt="Bonvin" />
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="https://www.bcv.ch/" target="_blank">
+          <img src="~/assets/images/partners/bcv.png" alt="BCV" />
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="http://eventente.ch/" target="_blank">
+          <img src="~/assets/images/partners/michot.png" alt="Michot Eventente" />
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.partners
+  display: flex
+  justify-content: space-around
+  list-style: none
+  margin: 0
+  padding: 0
+
+  img
+    height: 120px
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
   <HomeHero />
+  <HomePartners />
   <HomeProjects />
 </template>
